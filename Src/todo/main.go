d270348bbc6dcdad1f26e435317b8fbfff44fa66
Daniel Guzman <danielsonguzman2000@gmail.com>
2023-11-19 14:13:06 -0800

    added list flag

--- Src/todo/main.go
+++ Src/todo/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	todo "github.com/lostdanielfound/CLI-Todo-Go-App"
 )
@@ -14,7 +18,11 @@ const (
 )
 
 func main() {
+
 	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("delete", 0, "delete a todo item")
+	list := flag.Bool("list", false, "list all the todo items")
 
 	flag.Parse()
 
@@ -27,14 +35,65 @@ func main() {
 
 	switch {
 	case *add:
-		todos.Add("Sample todo")
-		err := todos.Store(todoFile)
+		task, err := getInput(os.Stdin, flag.Args()...)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(-1)
 		}
+
+		todos.Add(task)
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+		err = todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	case *list:
+		todos.Print()
 	default:
 		fmt.Fprintln(os.Stdout, "Invalid Command")
 		os.Exit(1)
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 { // Checks length of arguments
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	todoText := scanner.Text()
+
+	if len(todoText) == 0 { // scanner is empty
+		return "", errors.New("invalid todo: Empty string")
+	}
+
+	return todoText, nil
 }
