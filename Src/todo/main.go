0a4fa5bd18b5206d11f866ace71009e5a0b41df2
Daniel Guzman <danielsonguzman2000@gmail.com>
2023-11-19 13:32:06 -0800

    Added add flag

--- Src/todo/main.go
+++ Src/todo/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+
+	todo "github.com/lostdanielfound/CLI-Todo-Go-App"
 )
 
 // Program const variables
@@ -11,9 +14,27 @@ const (
 )
 
 func main() {
-	fmt.Println("Hello")
 	add := flag.Bool("add", false, "add a new todo")
+
+	flag.Parse()
 
 	todos := &todo.Todos{}
 
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(-1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(-1)
+		}
+	default:
+		fmt.Fprintln(os.Stdout, "Invalid Command")
+		os.Exit(1)
+	}
 }
